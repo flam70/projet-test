577fd363a339d434e95d0de83ca35727aca42fe7
Franck Lamy <FLamy@QUALIMS.LOCAL>
2017-10-16 11:21:14 +0200

    Commit initial

---
+++ src/main/java/com/qualims/git/ProjectGitApplication.java
@@ -0,0 +1,12 @@
+package com.qualims.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGitApplication.class, args);
+	}
+}
